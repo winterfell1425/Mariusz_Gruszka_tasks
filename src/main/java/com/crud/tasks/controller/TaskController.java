d62afbba557318ca0e9b416d1fcbf9b3e1680ec5
winterfell1425 <winterfell1425@gmail.com>
2019-02-05 20:17:51 +0100

    aaaaaaaa

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,7 +9,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
